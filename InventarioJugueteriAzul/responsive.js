d4b0dd044bd07f5f16b8becf68edf85e6073416a
Raul Polvo <polvoraul30@gmail.com>
2026-02-28 00:01:26 -0600

    Toggle menu icon on linksDiv active state
    Change menu icon based on active class in linksDiv.

--- InventarioJugueteriAzul/responsive.js
+++ InventarioJugueteriAzul/responsive.js
@@ -4,7 +4,7 @@ const linksDiv = document.querySelector('.links-div');
 menu.addEventListener('click', () => {
     linksDiv.classList.toggle('active');
 
-    // Si linksDiv tiene la clase 'active', cambiamos el ícono a 'times', si no, lo cambiamos a 'bars'
+    
     if (linksDiv.classList.contains('active')) {
         menu.classList.remove('fa-bars');
         menu.classList.add('fa-times');
